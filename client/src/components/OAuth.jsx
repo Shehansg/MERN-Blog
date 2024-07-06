79add11ba2e689128c3e480f5875d058c6ff514e
IT21284670 <it21284670@my.sliit.lk>
2024-07-06 18:06:02 +0530

    Google button color fix

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -34,11 +34,17 @@ export default function OAuth() {
         }
     } 
   return (
-    <Button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800" type='button' outline onClick={handleGoogleClick}>
-    <span className="relative px-2 py-0.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+    
+
+    <Button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800" type='button' gradientDuoTone='pinkToOrange' outline onClick={handleGoogleClick}>
+    
     <AiFillGoogleCircle className='relative inline-flex items-center justify-center w-6 h-6 mr-2'/>
     Continue with Google
-    </span>
+    
     </Button>
+
+    
   )
+  
+  
 }
